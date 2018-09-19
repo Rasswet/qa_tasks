439560b333a95d673334986d90363ff59e64fdeb
borschev_d <dimabdn@gmail.com>
2018-09-19 10:48:21 +0300

    # выполнено второе задание

---
+++ selen/task_02/file_01.py
@@ -0,0 +1,39 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
+
+
+def do_job():
+
+    chrome_options = Options()
+    chrome_options.add_argument("--window-size=1400,900")
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--log-level=DEBUG')
+    executable_path='C:\\PF\\selen_chrome\\chromedriver.exe'
+    browser = webdriver.Chrome(executable_path=executable_path,
+                               chrome_options=chrome_options)
+
+ 
+    browser.get("http://www.ya.ru")
+    assert "Яндекс" in browser.title
+
+ 
+    elem = WebDriverWait(browser, 10).until(lambda driver: driver.find_element_by_id("text"))
+
+    elem.send_keys("testing")
+    elem.send_keys(Keys.RETURN)
+    browser.implicitly_wait(10)  # seconds
+
+    # time.sleep(3)
+    filename="file.png"
+    browser.save_screenshot(filename)
+
+    browser.quit()
+
+if __name__ == '__main__':
+    do_job()
